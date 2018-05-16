032b2075a37dac925b8a1da75cecd82b23b97455
Matt Murnighan <mattmurn@gmail.com>
2018-05-16 13:53:50 -0500

    finished pigLatin challenge

--- CodeWars/codewars.test.js
+++ CodeWars/codewars.test.js
@@ -1,16 +1,24 @@
 // const filter_list = require('./listFilter') 
-// const friend = require('./friendFoe');
+//  const friend = require('./completed/friendFoe');
 // const validParens = require('./validParens')
-const recreationOne = require('./recreationOne');
+// const recreationOne = require('./recreationOne');
 
-const list = [1,'a','b',0,15];
-const test2 = [1,2,'aasf','1','123',123]
-const friendInput = ["Ryan", "Kieran", "Jason", "Yous"];
-const tester = "()(()))()";
+// const list = [1,'a','b',0,15];
+// const test2 = [1,2,'aasf','1','123',123]
+//  const friendInput = ["Ryan", "Kieran", "Jason", "Yous"];
+// const tester = "()(()))()";
+const pigIt = require('./pigIt');
 
-it("should return 4", () => {
-    expect(recreationOne(2, 2)).toEqual(4);
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+
+it("should return igPay atinlay siay oolcay", () => {
+    expect(pigIt("Pig latin is cool"))
+    .toEqual("igPay atinlay siay oolcay");
+    
 })
+// it("should return 4", () => {
+//     expect(recreationOne(2, 2)).toEqual(4);
+// })
 
 // it("should return false" , ()=> {
 
