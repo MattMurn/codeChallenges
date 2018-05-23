f9bc6eb2afd81b57604abd5e3fbb7ab0c5dd941b
Matt Murnighan <mattmurn@gmail.com>
2018-05-22 21:52:53 -0500

    fundamentals

--- CodeWars/codewars.test.js
+++ CodeWars/codewars.test.js
@@ -2,7 +2,6 @@
 //  const friend = require('./completed/friendFoe');
 // const validParens = require('./validParens')
 // const recreationOne = require('./recreationOne');
-
 // const list = [1,'a','b',0,15];
 // const test2 = [1,2,'aasf','1','123',123]
 //  const friendInput = ["Ryan", "Kieran", "Jason", "Yous"];
@@ -11,20 +10,34 @@
 // const add = require('./chainAdd');
 // const dfSq = require('./dfSq')
 // const listSquared = require('./recreationOne');
+// const bump = require('./bumps');
+// const aveng = require('./aveng');
+const getSum = require('./sums');
 
-const bump = require('./bumps');
+it("returns the sum of the every int in the range", () => {
 
-it("returns Wohoo if you complete the trip", () => {
-    expect(bump("nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn")).toEqual("Car Dead");
+    expect(getSum(-161,-177)).toEqual(-2873);
 })
 
 
 
 
+// const total = [25, 25, 50, 50, 1000]
+// it("Should return true", () => {
+
+//     expect(aveng(total)).toEqual("No");
+// })
 
 
 
 
+
+
+
+
+// it("returns Wohoo if you complete the trip", () => {
+//     expect(bump("nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn")).toEqual("Car Dead");
+// })
 
 // it("given two positive ints, find all integers b/t whose sum of sq divisors are sqs", () => {
 //     expect(listSquared(1, 250)).toEqual([ [ 1, 1 ], [ 42, 2500 ], [ 246, 84100 ] ])
