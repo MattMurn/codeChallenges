cabb78016b3858ec8989414fc9feefc2e8b0af4d
Matthew Murnighan <mattmurn@gmail.com>
2018-06-26 18:13:42 -0500

    mult table

--- CodeWars/multTable.js
+++ CodeWars/multTable.js
@@ -1,6 +1,7 @@
 multiplicationTable = (row, col)=>{
     // Each value on the table should be equal to the value of multiplying the number
     //  in its first row times the number in its first column.
+    // nested for loop unavoidable here
     let final = [];
     for(let i = 1; i <= row; i++){
         let rowA = []
@@ -11,5 +12,6 @@ multiplicationTable = (row, col)=>{
     }
     return final;
   }
+  
 
   module.exports = multiplicationTable;
