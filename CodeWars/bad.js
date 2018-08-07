e8ba8418f13fdb88af582a9032f0f91426d283bd
Matthew Murnighan <mattmurn@gmail.com>
2018-08-07 18:29:16 -0500

    updated redux exercise

--- CodeWars/bad.js
+++ CodeWars/bad.js
@@ -1,9 +1,30 @@
 function removeRotten(bagOfFruits){
     if(!bagOfFruits) { return []; }
-    
     return bagOfFruits.map(fruit => {
-      return fruit.replace("rotten", "").toLowerCase();
+      return console.log(fruit.replace("rotten", "").toLowerCase());
     });
   }
-  removeRotten(["rottenBanana", "apple", "kiwi", "qaii"])
-  removeRotten([])
+  // removeRotten(["rottenBanana", "apple", "kiwi", "qaii"])
+  // removeRotten([])
+
+
+  function Factory (name, number=0, age=0) {
+    this.name = name, 
+    this.number = number,
+    this.age = age
+
+    this.printInfo = () => {
+      console.log(` name :${this.name} number: ${this.number} age: ${age}`)
+    }
+  }
+
+const Matt = new Factory("Matt", 31278, 33);
+
+Matt.printInfo();
+
+Factory.prototype.hello = hello => {
+  console.log(`This is a factory function for saying ${hello}`)
+}
+
+Matt.hello("Goodbye")
+Matt.hello(`Hello again this is ${Matt.name}`)
